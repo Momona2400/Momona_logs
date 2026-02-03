508535b74a9b449dfee8675d53677c751745e95a
Momona <ananyaanuj2006@gmail.com>
2026-02-03 14:52:43 +0530

    dom

--- Java codes/Hellow.java
+++ Java codes/Hellow.java
@@ -1,7 +1,7 @@
 public class Hellow {
     public static void main(String[] args) {
         for(int i = 0; i < 5; i++) {
-            System.err.println("Hello");
+            System.out.println("Hello");
         }
     }
 }
